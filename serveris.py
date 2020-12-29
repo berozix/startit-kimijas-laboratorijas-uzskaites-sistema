15bde868634e4fdb15cafbd8b0a980d648cda206
berozix <berozix@gmail.com>
2020-12-29 11:02:21 +0000

    Vielas

--- serveris.py
+++ serveris.py
@@ -1,12 +1,24 @@
-from flask import Flask
-
+from flask import Flask, jsonify
+import dati
 
 app = Flask(__name__)
 
+app.config['JSON_AS_ASCII']  = False
 
 @app.route('/')
 def index():
     return "Labrīt!"
+
+@app.route ('/api/v1/vielas')
+def vielas():
+  return jsonify(dati.vielas)
+
+@app.route('/api/v1/viela/<vielasID>')
+def viela(vielasID):
+  for viela in dati.vielas
+    if v['ID'] == vielasID
+    viela = v
+  return jsonify(viela)
 
 
 if __name__ == "__main__":
